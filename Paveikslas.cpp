9875cf6cf5126476ae5261ca80835bcf09827a4b
DainiusFord <dainiusford@gmail.com>
2021-02-14 15:09:01 +0200

    Add files via upload

--- Paveikslas.cpp
+++ Paveikslas.cpp
@@ -5,15 +5,38 @@ using namespace std;
 int main()
 {
     string vardas;
+    string lytis;
+    int ilgis;
+    string eilute3;
+    bool teisinga = true;
     cout << "Koks jusu vardas? ";
     cin >> vardas;
-    int ilgis = vardas.length() + 14;
+    while (teisinga)
+    {
+        cout << "Kokia jusu lytis? (Vyras/Moteris) ";
+        cin >> lytis;
+        if (lytis == "Vyras")
+        {
+            ilgis = vardas.length() + 14;
+            eilute3 = "* Sveikas, " + vardas + "! *";
+            teisinga = false;
+        }
+        else if (lytis == "Moteris")
+        {
+            ilgis = vardas.length() + 13;
+            eilute3 = "* Sveika, " + vardas + "! *";
+            teisinga = false;
+        }
+        else
+        {
+            cout << "Ivedete bloga lyti..." << endl;
+        }
+    }
 
     string eilute1(ilgis, '*');
     string eilute2(ilgis, ' ');
     eilute2[0] = '*';
     eilute2[eilute2.length() - 1] = '*';
-    string eilute3("* Sveikas, " + vardas + "! *");
 
     cout << eilute1 << endl;
     cout << eilute2 << endl;
